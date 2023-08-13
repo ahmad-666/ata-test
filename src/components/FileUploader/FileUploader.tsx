8d0874e896acab130e655bb34127532d45aaacae
ahmad-666 <ahmadrezagghz666@gmail.com>
2023-08-13 13:07:19 +0330

    add input,file uploader

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { ChangeEvent, useCallback, useState } from "react";
+import styles from "./fileUploader.module.css";
 
-export default function FileUploader() {
-  return <div>FileUploader</div>;
+type FileUploaderProps = {
+  onChange: (file: File) => void;
+  label: string;
+  thumbnail: null | string;
+};
+export default function FileUploader({
+  onChange,
+  label,
+  thumbnail,
+}: FileUploaderProps) {
+  const [thumbnailSrc, setThumbnailSrc] = useState(thumbnail);
+  const changeHandler = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if (file) {
+        onChange(file);
+        const src = URL.createObjectURL(file);
+        setThumbnailSrc(src);
+      }
+    },
+    [onChange]
+  );
+  return (
+    <div>
+      <div>
+        <label className="primary p-lg rounded">
+          <p className="white--text text-md">{label}</p>
+          <input type="file" className="hidden" onChange={changeHandler} />
+        </label>
+      </div>
+      {thumbnailSrc && (
+        <img
+          src={thumbnailSrc}
+          alt="upload-img"
+          className={`mt-md rounded ${styles.thumbnail}`}
+        />
+      )}
+    </div>
+  );
 }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { useCallback } from "react";
 
-export default function InputField() {
-  return <div>InputField</div>;
+type InputFieldProps = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  label: string;
+  error?: null | string;
+  type?: string;
+};
+export default function InputField({
+  value,
+  setValue,
+  label,
+  error = null,
+  type = "text",
+}: InputFieldProps) {
+  const changeHandler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    },
+    [setValue]
+  );
+  return (
+    <div>
+      <div className="relative overflow-hidden rounded slate-light">
+        <input
+          type={type}
+          value={value}
+          onChange={changeHandler}
+          placeholder={label}
+        />
+        {error && <p className="text-sm error--text mt-sm"></p>}
+      </div>
+    </div>
+  );
 }
